c1ff5ea7046b0a44cbc0a73c853fea7c83efba6c
Kamesh Rathore <106003347+kameshrathore@users.noreply.github.com>
2024-09-30 15:24:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 db_params = {
-    'host': 'w3.training5.modak.com',
-    'database': 'postgres',
-    'user': 'mt24039',
-    'password': 'mt24039%40m06y24',
+    'host': 'enter',
+    'database': 'enter',
+    'user': 'enter',
+    'password': 'enter',
     'port': '5432'
 }
